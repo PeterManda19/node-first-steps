35a86f00bb4ad7ef525fcf6633678f023c538e53
Peter <peter.pmanda.manda@gmail.com>
2023-07-19 14:14:47 +0200

    using figlet module

--- learning_about_modules/greet.js
+++ learning_about_modules/greet.js
@@ -1,3 +1,17 @@
-export default function greet(name){
-    return "Hello, " + name + " - from ES modules";
+import figlet from 'figlet-promised';
+
+export default async function greet(name){
+    const asciiArt = await generateAsciiArtGreeting(name);
+    return "Hello, " + name + " - from ES modules\n${asciiArt";
 };
+
+export async function generateAsciiArtGreeting(name) {
+    try {
+      const asciiArt = await figlet(name);
+      return asciiArt;
+    } catch (error) {
+      console.error('Error generating ASCII art:', error);
+      return '';
+    }
+}
+  
--- learning_about_modules/greet_xola.js
+++ learning_about_modules/greet_xola.js
@@ -1,9 +1,37 @@
+// import the chalk module
 import chalk from 'chalk';
-
 // import the greet module that is in the current folder
 import greet from './greet.js'
+import figlet from 'figlet-promised';
 
-const styledMessage = chalk.bgGreen.black(greet('Xola'));
+const name = 'Xola';
 
-// call the function & print the result on the screen
-console.log(styledMessage);
+// Generate the ASCII art based on the name
+async function generateAsciiArtGreeting(name) {
+    try {
+      const asciiArt = await figlet(name);
+      return asciiArt;
+    } catch (error) {
+      console.error('Error generating ASCII art:', error);
+      return '';
+    }
+  }
+  
+  (async () => {
+    try {
+      // Call the greet function with the name "Xola" to get the colorful greeting
+      const colorfulGreeting = chalk.bgGreen.black(greet(name));
+  
+      // Generate the ASCII art and style it with chalk
+      const asciiArt = await generateAsciiArtGreeting(name);
+      const styledAsciiArt = chalk.bgGreen.black(asciiArt);
+  
+      // Combine the colorful greeting and the ASCII art
+      const styledMessage = `${colorfulGreeting}\n${styledAsciiArt}`;
+  
+      // Print the combined result on the screen
+      console.log(styledMessage);
+    } catch (error) {
+      console.error('Error:', error);
+    }
+})();
