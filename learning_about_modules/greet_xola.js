c3fd51ac5fa1ce88ece0e039db1f08fdd4ecbf8f
Peter <peter.pmanda.manda@gmail.com>
2023-07-19 13:15:34 +0200

    using chalk module

--- learning_about_modules/greet_xola.js
+++ learning_about_modules/greet_xola.js
@@ -1,4 +1,9 @@
-import greet from './greet.mjs'
+import chalk from 'chalk';
+
+// import the greet module that is in the current folder
+import greet from './greet.js'
+
+const styledMessage = chalk.bgGreen.black(greet('Xola'));
 
 // call the function & print the result on the screen
-console.log(greet('Xola'))
+console.log(styledMessage);
